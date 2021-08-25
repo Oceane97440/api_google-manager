a89901e4d2cda7b6b71d2c609dd4281431617c8e
oceane97440 <sautronoceane98@gmail.com>
2021-08-25 15:18:01 +0400

    var_dump(key($reportJob));

--- Report.php
+++ Report.php
@@ -59,7 +59,7 @@ class RunSavedQuery
       $reportJob = new ReportJob();
       $reportJob->setReportQuery($reportQuery);
       $reportJob = $reportService->runReportJob($reportJob);
-      var_dump($reportJob);
+      var_dump(key($reportJob));
 
        /* // Optionally modify the query.
         $reportQuery->setAdUnitView(ReportQueryAdUnitView::HIERARCHICAL);
