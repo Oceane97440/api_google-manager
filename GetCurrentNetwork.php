db67c1e8ac2bdf6311160d5eb68e856015febb13
oceane97440 <sautronoceane98@gmail.com>
2021-08-26 09:47:15 +0400

    test

--- GetCurrentNetwork.php
+++ GetCurrentNetwork.php
@@ -36,4 +36,79 @@ printf(
     $network->getDisplayName()
 );
 
+class RunSavedQuery
+{
+
+    const SAVED_QUERY_ID = '12304979660';
+
+    public static function runExample(
+        ServiceFactory $serviceFactory,
+        AdManagerSession $session,
+        int $savedQueryId
+    ) {
+        $reportService = $serviceFactory->createReportService($session);
+
+        // Create statement to retrieve the saved query.
+        $statementBuilder = (new StatementBuilder())->where('id = :id')
+            ->orderBy('id ASC')
+            ->limit(1)
+            ->withBindVariableValue('id', $savedQueryId);
+
+        $savedQueryPage = $reportService->getSavedQueriesByStatement(
+            $statementBuilder->toStatement()
+        );
+        $savedQuery = $savedQueryPage->getResults()[0];
+
+        if ($savedQuery->getIsCompatibleWithApiVersion() === false) {
+            throw new UnexpectedValueException(
+               print('The saved query is not compatible with this API version.')
+            );
+        }else{
+            print('Request ready');
+  
+        } 
+        $reportQuery = $savedQuery->getReportQuery();
+
+        // Create report job and start it.
+      $reportJob = new ReportJob();
+      $reportJob->setReportQuery($reportQuery);
+      $reportJob = $reportService->runReportJob($reportJob);
+
+
+  
+      // Create report downloader to poll report's status and download when
+      // ready.
+      $reportDownloader = new ReportDownloader(
+        $reportService,
+        $reportJob->getId()
+    );
+
+    var_dump($reportDownloader);  
+
+/*
+    if (!$reportDownloader->waitForReportToFinish()) {
+
+    }*/
+
+
+    
+    }
+
+    public static function main()
+    {
+        $oAuth2Credential = (new OAuth2TokenBuilder())->fromFile()
+            ->build();
+        $session = (new AdManagerSessionBuilder())->fromFile()
+            ->withOAuth2Credential($oAuth2Credential)
+            ->build();
+        self::runExample(
+            new ServiceFactory(),
+            $session,
+            intval(self::SAVED_QUERY_ID)
+        );
+    }
+}
+
+RunSavedQuery::main();
+
 
