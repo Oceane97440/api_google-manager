28d7a5b54a88d5510d1b4ac5eda424fde6660b63
Oceane97440 <sautronoceane98@gmail.com>
2021-12-10 16:10:23 +0400

    MAJ reporting gam

--- reporting.php
+++ reporting.php
@@ -181,7 +181,66 @@ if ($reportDownloader->waitForReportToFinish()) {
 
             rename($file_exist,'data/csv/'.date('Y/m/d').'/campaignID-fileAll.csv');
             unlink($file_name);
+
+
+        // lecture des fichiers csv
+        $file_csv = 'data/csv/'.date('Y/m/d').'/campaignID-fileAll.csv';
+
+    if (file_exists($file_csv)) {
+
+        $srcFile = new SplFileObject($file_csv);
+        foreach ($srcFile as $line) {
+            $item = explode(',',$line);
+
+            if(count($item) > 1) {
+                $dataArray[] = $item;
+            }
         }
+
+        $campaigns = array();
+        if(!empty($dataArray)) {
+
+        $columnKey = $dataArray[0];
+        $keyPrimary = $columnKey[0]; 
+
+        foreach($dataArray as $k => $i) {
+
+            if((count($columnKey) === count($i)) && ($k != 0)) {  
+
+                $newItem = array(); 
+                for($y = 0; $y < count($i); $y++) {
+                    $newItem[trim($columnKey[$y])] = trim($i[$y]);
+                }
+            }
+            if(!empty($newItem) ) {
+
+                $valuePrimary = $newItem[$keyPrimary];
+                $campaigns[$valuePrimary][] = $newItem;
+            }       
+        }      
+        if (!empty($campaigns) && (count($campaigns) > 0)) {
+            foreach ($campaigns  as $key => $value){
+                echo $key;
+
+                $fp = fopen('data/csv/'.date('Y/m/d').'/campaignID-'.$key.'.csv', 'w');
+                $arrayLabels = array_keys($value[0]);
+
+                fputcsv($fp, $arrayLabels);
+
+                foreach ($value as $key0 => $value0) {
+                    fputcsv($fp,$value0);
+                    var_dump($value0);
+                }            
+                fclose($fp);
+            }         
+        }
+        echo '<hr />';
+    }
+
+    }
+            
+
+}
 
    
 
